12fba365f53bdd29439ffb39bd77481a36110f85
alex <alex.stirz@gmail.com>
2023-04-07 17:44:34 +0200

    Added test for WeatheyDay class

---
+++ src/test/java/de/bcxp/challenge/weather/WeatherDayTest.java
@@ -0,0 +1,26 @@
+package de.bcxp.challenge.weather;
+
+import de.bcxp.challenge.weather.WeatherDay;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Example JUnit 5 test case.
+ */
+class WeatherDayTest {
+
+    @Test
+    public void testGetTemperatureDifference() {
+        WeatherDay day1 = new WeatherDay(1, 10, 20);
+        assertEquals(10, day1.getTemperatureDifference());
+
+        WeatherDay day2 = new WeatherDay(2, 15, 20);
+        assertEquals(5, day2.getTemperatureDifference());
+
+        WeatherDay day3 = new WeatherDay(3, 20, 10);
+        assertEquals(10, day3.getTemperatureDifference());
+    }
+
+}
