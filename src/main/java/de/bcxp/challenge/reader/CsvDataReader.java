8248f6bb5be829d3cb77e1a8c2006e6925bee8a2
alex <alex.stirz@gmail.com>
2023-04-07 14:00:00 +0200

    implemented class 'CsvDataReader' with corresponding interface 'DataReader'

---
+++ src/main/java/de/bcxp/challenge/reader/CsvDataReader.java
@@ -0,0 +1,42 @@
+package de.bcxp.challenge.reader;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+
+import java.io.FileReader;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class CsvDataReader implements DataReader<String[]>{
+
+
+    /**
+     *
+     * @param filePath path of the input csv file
+     * @return a list of string with data from csv
+     */
+    @Override
+    public List<String[]> readData(String filePath){
+
+        List<String[]> allData = null;
+
+        try{
+            FileReader filereader = new FileReader(filePath);
+
+            CSVReader csvReader = new CSVReaderBuilder(filereader)
+                                        .withSkipLines(1)
+                                        .build();
+
+            allData = csvReader.readAll();
+
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
+        return allData;
+    }
+
+}
